d3028c5f432bddfd0a32935051f9a13477bb4b90
yinghang.fang <yinghang.fang@kunlun-inc.com>
2025-09-30 00:35:28 +0800

    添加编辑器配置文件并更新初始化配置

--- init.lua
+++ init.lua
@@ -155,6 +155,28 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
+--
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
